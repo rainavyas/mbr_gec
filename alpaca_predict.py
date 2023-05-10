ae73d3f05a730ed0a3d27a7f57d366fe3a99f3ce
Vyas Raina <sayvaniar@gmail.com>
2023-05-10 17:57:21 +0100

    preds

--- alpaca_predict.py
+++ alpaca_predict.py
@@ -47,13 +47,16 @@ if __name__ == '__main__':
     with open('CMDs/alpaca_predict.cmd', 'a') as f:
         f.write(' '.join(sys.argv)+'\n')
     
+    # load input file
+    with open(args.input_file, 'r') as f:
+        lines = f.readlines()
+    lines = [l.rstrip('\n') for l in lines]
+    ids = [l.split(' ')[0] for l in lines]
+    texts = [' '.join(l.split(' ')[1:]) for l in lines]
+
+    # load model
     tokenizer = LlamaTokenizer.from_pretrained("chainyo/alpaca-lora-7b")
-    model = LlamaForCausalLM.from_pretrained(
-        "chainyo/alpaca-lora-7b",
-        # load_in_8bit=True,
-        # torch_dtype=torch.float16,
-        # device_map="auto",
-    )
+    model = LlamaForCausalLM.from_pretrained("chainyo/alpaca-lora-7b")
     generation_config = GenerationConfig(
         temperature=0.2,
         top_p=0.75,
@@ -65,23 +68,34 @@ if __name__ == '__main__':
     model.eval()
     if torch.__version__ >= "2":
         model = torch.compile(model)
-
+    
+    # create predictions
+    preds = []
     instruction = "Grammatically correct the following?"
-    input_ctxt = 'I is run in park.'
-    prompt = generate_prompt(instruction, input_ctxt)
+    for idd, text in zip(ids, texts):
+        prompt = generate_prompt(instruction, text)
 
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-    input_ids = input_ids.to(model.device)
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+        input_ids = input_ids.to(model.device)
 
-    with torch.no_grad():
-        outputs = model.generate(
-            input_ids=input_ids,
-            generation_config=generation_config,
-            return_dict_in_generate=True,
-            output_scores=True,
-        )
+        with torch.no_grad():
+            outputs = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+            )
 
-    response = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
-    import pdb; pdb.set_trace()
-    print(response)
+        response = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
+        loc = response.find('Response')
+        pred = response[loc+18:]
+        import pdb; pdb.set_trace()
+
+        preds.append(idd + ' '+ pred)
+    
+    # Save to output file
+    with open(args.output_file, 'w') as f:
+        f.writelines(preds)
+
+
 
