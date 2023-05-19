06e228c0d47592bb06d46572666f78a8a99d276b
Vyas Raina <sayvaniar@gmail.com>
2023-05-19 22:28:28 +0100

    funcs

--- mbr_diff.py
+++ mbr_diff.py
@@ -15,86 +15,9 @@ import os
 import argparse
 from tqdm import tqdm
 import errant
+from mbr import edit_agreement, edit_jaccard, edit_f05, edit_prec, edit_rec, return_edits
 
 annotator = errant.load('en')
-
-
-def return_edits(input, prediction):
-    '''
-    Get edits
-    '''
-    input = annotator.parse(input)
-    prediction = annotator.parse(prediction)
-    alignment = annotator.align(input, prediction)
-    edits = annotator.merge(alignment)
-    for e in edits:
-        e = annotator.classify(e)
-    return edits
-
-def edit_agreement(edits1, edits2):
-    '''
-        Number of matching edits 
-    '''
-    edits1_str = [e.o_str+' -> '+e.c_str for e in edits1]
-    edits2_str = [e.o_str+' -> '+e.c_str for e in edits2]
-
-    matched = 0
-    for e1_str in edits1_str:
-        if e1_str in edits2_str:
-            matched += 1
-    return matched
-
-def edit_jaccard(edits1, edits2):
-    list1 = [e.o_str+' -> '+e.c_str for e in edits1]
-    list2 = [e.o_str+' -> '+e.c_str for e in edits2]
-
-    if len(list1) == 0 and len(list2) == 0:
-        return 1
-
-    intersection = len(list(set(list1).intersection(list2)))
-    union = (len(list1) + len(list2)) - intersection
-
-    return float(intersection) / union
-
-def edit_rec(edits1, edits2):
-
-    # rec estimate based reward
-    # edits1: ref 
-    # edits2: hyp
-    list1 = [e.o_str+' -> '+e.c_str for e in edits1]
-    list2 = [e.o_str+' -> '+e.c_str for e in edits2]
-
-    if len(list1) == 0:
-        return 1
-
-    intersection = len(list(set(list1).intersection(list2)))
-    return float(intersection) / len(list1)
-
-def edit_prec(edits1, edits2):
-
-    # prec estimate based reward
-    # edits1: ref 
-    # edits2: hyp
-    list1 = [e.o_str+' -> '+e.c_str for e in edits1]
-    list2 = [e.o_str+' -> '+e.c_str for e in edits2]
-
-    if len(list2) == 0:
-        return 1
-
-    intersection = len(list(set(list1).intersection(list2)))
-    return float(intersection) / len(list2)
-
-def edit_f05(edits1, edits2):
-    # f05 estimate based reward
-    # edits1: ref 
-    # edits2: hyp
-    k = 0.5
-    list1 = [e.o_str+' -> '+e.c_str for e in edits1]
-    list2 = [e.o_str+' -> '+e.c_str for e in edits2]
-    if len(list1 + list2) == 0:
-        return 1
-    intersection = len(list(set(list1).intersection(list2)))
-    return ((1+(k**2))*intersection)/((k*len(list1)) + len(list2))
 
 
 if __name__ == "__main__":
